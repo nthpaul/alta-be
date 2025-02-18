fdcd008cdc4a932a24b089db04f298e395a74ddd
Paul Le <nthpaul@proton.me>
2025-02-19 01:00:14 +1100

    chore: update CORS for vercel client

--- app/main.py
+++ app/main.py
@@ -4,12 +4,18 @@ from app.routers import search
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://alta-fe.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
     allow_credentials=True,
 )
 
 app.include_router(search.router)
+
