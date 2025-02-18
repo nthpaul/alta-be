faa1c135b36a994c2bf61b3e09b416bab579d794
Paul Le <nthpaul@proton.me>
2025-02-19 00:42:24 +1100

    feat: redis key for deployment

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 
 SERP_API_KEY = os.environ.get("SERP_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0") 
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
